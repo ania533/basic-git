0401c752837a2668ac9ae1bfc5785e4e4c3e6780
Ania <ania.k33@wp.pl>
2020-02-22 10:04:39 +0100

    first commit in person branch

--- src/academy/online11/Main.java
+++ src/academy/online11/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Kod główny bez brancha");
 
+        System.out.println("siema siema to jest branch person" );
     }
 }
